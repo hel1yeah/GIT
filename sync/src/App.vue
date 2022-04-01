9c0ca38cd4375b208d9766ee7670b2cd0a859d5c
hel1_yeah <hel1_yeah@ukr.net>
2022-04-01 12:34:11 +0300

    govno

--- sync/src/App.vue
+++ sync/src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <SyncTest v-model:inputValue></SyncTest>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SyncTest from "./components/SyncTest.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    SyncTest,
+  },
+  data() {
+    return {
+      inputValue: "test",
+    };
   },
 };
 </script>
---
+++ sync/src/components/SyncTest.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <input @input="changeInput($event.target.value)" value="sdf" type="text" />
+    <h2>input tets</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SyncTest",
+  props: ["inputValue"],
+  methods: {
+    changeInput(e) {
+      console.log(e);
+      this.$emit("update:inputValue", e);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 500px;
+  margin: 0 auto;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
