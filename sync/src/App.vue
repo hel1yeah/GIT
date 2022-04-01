676319b21807c2da65e91cfb56b7d5edd2f19cce
hel1_yeah <hel1_yeah@ukr.net>
2022-04-01 13:04:05 +0300

    test-sync

--- sync/src/App.vue
+++ sync/src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    {{textInput}}
+    <HelloWorld :textInput.sync="textInput" msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
@@ -10,11 +13,16 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      textInput: 'test'
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
